64c410bdf264954826fc28589de1946af1bd5dca
jysh <3045033935@qq.com>
2018-03-20 15:25:24 +0800

    .

--- app/src/main/java/jysh/mf/Activity/StaticText.java
+++ app/src/main/java/jysh/mf/Activity/StaticText.java
@@ -1,7 +1,14 @@
 package jysh.mf.Activity;
 
 import android.app.*;
+import android.os.*;
 
 public class StaticText extends Activity
 {
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
 }
